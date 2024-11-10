f431013510ca079b9e2a4d93fcb397104dbef582
Niki Yoshiuchi <nyoshiuchi@gmail.com>
2024-11-09 20:10:10 -0800

    Adding circles and point generation
    Generating equilateral triangle points and adding code to plot circles
    around each triangle

--- voronoi.py
+++ voronoi.py
@@ -10,15 +10,44 @@ points = np.array([[0.56308613, 0.25387432],
        [0.40419715, 0.43198382],
        [0.20983254, 0.86107708]])
 
-def plot(points):
-    tri = Delaunay(points)
-    vor = Voronoi(points)
 
+def create_points():
+    points = []
+    y_delta = np.sqrt(3)
+    for y in range(8):
+        for x in range(8):
+            if y % 2 == 0:
+                points.append([x*2, y_delta*y])
+            else:
+                points.append([x*2 + 1, y_delta*y])
+
+    return np.array(points)
+
+def plot(points):
     # plot the points
     plt.plot(points[:, 0], points[:, 1], 'o')
 
+    plot_delaunay(points)
+
+    plot_voronoi(points)
+            
+    plt.axis('equal')
+    plt.show()
+
+
+def plot_delaunay(points):
+    tri = Delaunay(points)
+
     # plot the Delaunay triangulation
     plt.triplot(points[:, 0], points[:, 1], tri.simplices)
+
+    # plot the circles around the triangles
+    for s in tri.simplices:
+        circle(points[s])
+
+
+def plot_voronoi(points):
+    vor = Voronoi(points)
 
     # plot the finite Voronoi diagram
     for i, ridge in enumerate(vor.ridge_vertices):
@@ -31,8 +60,6 @@ def plot(points):
     for i, ridge in enumerate(vor.ridge_vertices):
         if -1 in ridge:
             perp(vor, i)
-            
-    plt.show()
 
 
 def perp(vor, index):
@@ -62,4 +89,26 @@ def perp(vor, index):
     ys = [vor.vertices[i, 1], far_point[1]]
     plt.plot(xs, ys, color='k')
 
+
+def circle(triangle):
+    p1 = triangle[0]
+    p2 = triangle[1]
+    p3 = triangle[2]
+ 
+    temp = p2[0] * p2[0] + p2[1] * p2[1]
+    bc = (p1[0] * p1[0] + p1[1] * p1[1] - temp) / 2
+    cd = (temp - p3[0] * p3[0] - p3[1] * p3[1]) / 2
+    det = (p1[0] - p2[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p2[1])
+    
+    if abs(det) < 1.0e-6:
+        return (None, np.inf)
+    
+    # Center of circle
+    cx = (bc*(p2[1] - p3[1]) - cd*(p1[1] - p2[1])) / det
+    cy = ((p1[0] - p2[0]) * cd - (p2[0] - p3[0]) * bc) / det
+    
+    radius = np.sqrt((cx - p1[0])**2 + (cy - p1[1])**2)
+
+    circle = plt.Circle((cx, cy), radius, fill=False, color='g')
+    plt.gca().add_patch(circle)
 
