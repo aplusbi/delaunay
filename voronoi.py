21490618eeaa25f6e606b510d19fa41dbdb8168e
Niki Yoshiuchi <nyoshiuchi@gmail.com>
2024-11-17 12:12:14 -0800

    Circles and subplots
    Creating subplots with different combinations of Delaunay triangles,
    circles and Voronoi diagrams.

--- voronoi.py
+++ voronoi.py
@@ -3,6 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from scipy.spatial import Delaunay
 from scipy.spatial import Voronoi
+import math as math
 matplotlib.use('gtk3agg')
 
 points = np.array([[0.56308613, 0.25387432],
@@ -11,42 +12,71 @@ points = np.array([[0.56308613, 0.25387432],
        [0.20983254, 0.86107708]])
 
 
-def create_points():
+def create_points(noise):
+    mean = (0, 0)
+    cov = [[noise, 0], [0, noise]]
+    uncerts = np.random.multivariate_normal(mean, cov, (8, 8))
     points = []
     y_delta = np.sqrt(3)
     for y in range(8):
         for x in range(8):
+            xp = x*2
+            yp = y_delta*y
+
+            if y > 0 and y < 7:
+                yp += uncerts[x, y, 1]
+
+            if x > 0 and x < 7:
+                xp += uncerts[x, y, 0]
+
             if y % 2 == 0:
-                points.append([x*2, y_delta*y])
-            else:
-                points.append([x*2 + 1, y_delta*y])
+                points.append([xp, yp])
+            elif x < 7:
+                points.append([xp+1, yp])
 
     return np.array(points)
 
 def plot(points):
+    fig, axs = plt.subplots(2, 3, sharex=True, sharey=True)
+    ((pfig, triangles, circles), (tricircles, voronoi, combined)) = axs
+
     # plot the points
-    plt.plot(points[:, 0], points[:, 1], 'o')
+    plot_points(axs.flatten(), points)
 
-    plot_delaunay(points)
+    tri = plot_delaunay([triangles, tricircles, combined], points)
 
-    plot_voronoi(points)
+    plot_circles([circles, tricircles, combined], points, tri)
+
+    plot_voronoi([voronoi, combined], points)
             
-    plt.axis('equal')
+    for ax in axs.flat:
+        ax.set_aspect('equal')
     plt.show()
 
 
-def plot_delaunay(points):
+def plot_points(axs, points):
+    # plot the points
+    for ax in axs:
+        ax.plot(points[:, 0], points[:, 1], 'o')
+
+
+def plot_delaunay(axs, points):
     tri = Delaunay(points)
 
     # plot the Delaunay triangulation
-    plt.triplot(points[:, 0], points[:, 1], tri.simplices)
+    for ax in axs:
+        ax.triplot(points[:, 0], points[:, 1], tri.simplices)
 
+    return tri
+
+
+def plot_circles(axs, p, tri):
     # plot the circles around the triangles
     for s in tri.simplices:
-        circle(points[s])
+        circle(axs, p[s])
 
 
-def plot_voronoi(points):
+def plot_voronoi(axs, points):
     vor = Voronoi(points)
 
     # plot the finite Voronoi diagram
@@ -54,15 +84,16 @@ def plot_voronoi(points):
         if -1 not in ridge:
             polygon = [vor.vertices[j] for j in ridge]
             xs, ys = zip(*polygon)
-            plt.plot(xs, ys, color='k')
+            for ax in axs:
+                ax.plot(xs, ys, color='k')
 
     # plot the infinite Voronoi diagram
     for i, ridge in enumerate(vor.ridge_vertices):
         if -1 in ridge:
-            perp(vor, i)
+            perp(axs, vor, i)
 
 
-def perp(vor, index):
+def perp(axs, vor, index):
     """
     Code taken from scipy voronoi_plot_2d
     """
@@ -87,10 +118,12 @@ def perp(vor, index):
 
     xs = [vor.vertices[i, 0], far_point[0]]
     ys = [vor.vertices[i, 1], far_point[1]]
-    plt.plot(xs, ys, color='k')
+
+    for ax in axs:
+        ax.plot(xs, ys, color='k')
 
 
-def circle(triangle):
+def circle(axs, triangle):
     p1 = triangle[0]
     p2 = triangle[1]
     p3 = triangle[2]
@@ -108,7 +141,26 @@ def circle(triangle):
     cy = ((p1[0] - p2[0]) * cd - (p2[0] - p3[0]) * bc) / det
     
     radius = np.sqrt((cx - p1[0])**2 + (cy - p1[1])**2)
+    
+    for ax in axs:
+        circle = plt.Circle((cx, cy), radius, fill=False, color='g')
+        ax.add_patch(circle)
 
-    circle = plt.Circle((cx, cy), radius, fill=False, color='g')
-    plt.gca().add_patch(circle)
 
+def circle_points(r, n):
+    angle = 2*np.pi/n
+    points = []
+    for i in range(0, n):
+        x = r*math.cos(i*angle)
+        y = r*math.sin(i*angle)
+        points.append([x, y])
+
+    return points
+
+
+def create_circle_points(num_circles, num_points, points_delta):
+    points = [[0, 0]]
+    for i in range(1, num_circles):
+        points += circle_points(i, num_points + num_points*(i-1)*points_delta)
+
+    return np.array(points)
